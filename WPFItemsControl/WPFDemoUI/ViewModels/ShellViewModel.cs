67a06fafb325e0c168f5c1fa044e82c24d5fc6f8
toth.tozso.zoltan <toth.tozso.zoltan@gmail.com>
2020-05-03 21:42:37 +0200

    Part5 - WPF Controls with MVVM: Add and Remove item

--- WPFItemsControl/WPFDemoUI/ViewModels/ShellViewModel.cs
+++ WPFItemsControl/WPFDemoUI/ViewModels/ShellViewModel.cs
@@ -12,11 +12,33 @@ namespace WPFDemoUI.ViewModels
     public class ShellViewModel
     {
         public BindableCollection<PersonModel> People { get; set; }
+        DataAccess dataAccess = new DataAccess();
 
         public ShellViewModel()
         {
-            DataAccess da = new DataAccess();
-            People = new BindableCollection<PersonModel>(da.GetPeople());
+            //DataAccess dataAccess = new DataAccess();
+            People = new BindableCollection<PersonModel>(dataAccess.GetPeople());
+        }
+
+        public void AddPerson()
+        {
+            //DataAccess dataAccess = new DataAccess();
+
+            int maxId = 0;
+            if(People.Count > 0)
+                maxId = People.Max(p => p.PersonId);
+
+            People.Add(dataAccess.GetPerson(maxId + 1));
+        }
+
+        public void RemovePerson()
+        {
+            if (People.Count == 0)
+            {
+                return;
+            }
+            //DataAccess dataAccess = new DataAccess();
+            People.Remove(dataAccess.GetRandomItem(People.ToArray()));
         }
     }
 }
